3964a71e96ead29d13a61607010b5cc8abed3644
maryamzurmi <maryamzurmi02@gmail.com>
2025-06-23 12:48:39 +0100

    update task tracker: added togglestatus,editTask, saveEditedTask, cancelEdit, updatelocalstorage, saveTaskstolocalstorage, Fixed UI

--- src/app/task-manager/task-manager.component.ts
+++ src/app/task-manager/task-manager.component.ts
@@ -12,7 +12,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class TaskManagerComponent {
   newTask: string = '';
-  tasks: string[] = [];
+  tasks: Array<{ name: string; completed: boolean }> = [];
+
+  editIndex: number | null = null;
+  editedTaskName: string = '';
+
 
   ngOnInit(): void {
     const savedTasks = localStorage.getItem('tasks');
@@ -23,7 +27,7 @@ export class TaskManagerComponent {
 
   addTask() {
     if (this.newTask.trim()) {
-      this.tasks.push(this.newTask.trim());
+      this.tasks.push({ name: this.newTask.trim(), completed: false });
       this.newTask = '';
       this.updateLocalStorage();
     }
@@ -33,8 +37,34 @@ export class TaskManagerComponent {
     this.tasks.splice(index, 1);
     this.updateLocalStorage();
   }
+  toggleStatus(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+    this.updateLocalStorage();
+  }
+
+  editTask(index: number) {
+    this.editIndex = index;
+    this.editedTaskName = this.tasks[index].name;
+  }
+
+  saveEditedTask() {
+    if (this.editIndex !== null) {
+      this.tasks[this.editIndex].name = this.editedTaskName.trim();
+      this.editIndex = null;
+      this.editedTaskName = '';
+      this.updateLocalStorage();
+    }
+  }
+
+  cancelEdit() {
+    this.editIndex = null;
+    this.editedTaskName = '';
+  }
 
   updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+  saveTasksToLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
